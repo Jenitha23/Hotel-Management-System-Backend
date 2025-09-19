4b1fbba720b0b575a5a9cc6d04762c50fe8d6fca
J-Jenitha <jenithamaxi17@gmail.com>
2025-09-19 11:49:53 +0530

    Delete Scrum11 directory

--- Scrum11/src/main/java/com/hotelmanagement/scrum11/rooms/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hotelmanagement.scrum11.rooms;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface RoomRepository
-        extends JpaRepository<Room, Long>, JpaSpecificationExecutor<Room> {
-}
--- Scrum11/src/main/java/com/hotelmanagement/scrum11/rooms/RoomType.java
+++
@@ -1,3 +0,0 @@
-package com.hotelmanagement.scrum11.rooms;
-
-public enum RoomType { STANDARD, DELUXE, SUITE, FAMILY }
