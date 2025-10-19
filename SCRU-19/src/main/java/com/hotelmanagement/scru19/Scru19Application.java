292b82e45de343c9dc8fe24af747b29aa56c5dc5
SANO58 <it23619180@my.sliit.lk>
2025-10-19 17:53:16 +0530

    SCRU-19: Add dependencies into the pom.xml file

---
+++ SCRU-19/src/main/java/com/hotelmanagement/scru19/Scru19Application.java
@@ -0,0 +1,13 @@
+package com.hotelmanagement.scru19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Scru19Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Scru19Application.class, args);
+    }
+
+}
