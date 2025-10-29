9231e5caee7386d99e1bcdccb66f01fa08d70b1b
J-Jenitha <jenithamaxi17@gmail.com>
2025-10-29 11:24:19 +0530

    Delete HotelManagement directory

--- HotelManagement/src/main/java/com/hotelmanagement/error/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.hotelmanagement.error;
-
-
-/**
- * 404 marker exception.
- */
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) { super(message); }
-}
