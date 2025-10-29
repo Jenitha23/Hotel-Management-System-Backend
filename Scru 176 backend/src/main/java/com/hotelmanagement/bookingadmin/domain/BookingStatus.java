e60f4b4b154b4616fdc941a8e97b46d54ecfd8f9
J-Jenitha <jenithamaxi17@gmail.com>
2025-10-29 11:11:47 +0530

    Delete Scru 176 backend directory

--- Scru 176 backend/src/main/java/com/hotelmanagement/bookingadmin/domain/BookingStatus.java
+++
@@ -1,6 +0,0 @@
-package com.hotelmanagement.bookingadmin.domain;
-
-
-public enum BookingStatus {
-    PENDING, CONFIRMED, CANCELLED, COMPLETED
-}
--- Scru 176 backend/src/main/java/com/hotelmanagement/bookingadmin/service/BadRequestException.java
+++
@@ -1,5 +0,0 @@
-package com.hotelmanagement.bookingadmin.service;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String msg) { super(msg); }
-}
