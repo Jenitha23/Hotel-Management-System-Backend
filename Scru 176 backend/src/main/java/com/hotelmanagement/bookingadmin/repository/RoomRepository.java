d2cc72e0972e90450de92b65ea46def9d3f5d552
J-Jenitha <jenithamaxi17@gmail.com>
2025-10-29 11:20:08 +0530

    Delete Scru 176 backend directory

--- Scru 176 backend/src/main/java/com/hotelmanagement/bookingadmin/repository/RoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hotelmanagement.bookingadmin.repository;
-
-
-import com.hotelmanagement.bookingadmin.domain.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room, Long> {}
