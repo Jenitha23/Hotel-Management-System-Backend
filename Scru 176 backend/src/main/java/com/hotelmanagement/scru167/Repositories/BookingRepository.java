f822867f43b5d6067dbbc01365bcda5b6f6e0883
J-Jenitha <jenithamaxi17@gmail.com>
2025-10-29 11:17:43 +0530

    Delete Scru 176 backend directory

--- Scru 176 backend/src/main/java/com/hotelmanagement/scru167/Repositories/BookingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hotelmanagement.scru167.Repositories;
-
-
-import com.hotelmanagement.scru167.domain.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {}
