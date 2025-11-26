eb2e0e5b1e52fa197ddb6e5fdfb467eed2e55269
it23596566 <it23596566@my.sliit.lk>
2025-11-26 10:57:37 +0530

    change SecurityConfig

--- palmbeachresortBackend/src/main/java/com/palmbeachresort/config/SecurityConfig.java
+++ palmbeachresortBackend/src/main/java/com/palmbeachresort/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.palmbeachresort.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -51,6 +52,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         // Public endpoints
                         .requestMatchers(
                                 "/api/customers/auth/register",
